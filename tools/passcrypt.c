e63b1fc2b7019e99aec1082285c42219727f548f
Andrew Clayton <andrew@opentechlabs.co.uk>
2011-05-31 13:39:44 +0100

    Add a crypt'd passwd generator
    Add a tool to create password entries for the passwd table.
    
    The tool can create entries using either the SHA-256 or SHA-512
    algorithms within crypt.
    
    Signed-off-by: Andrew Clayton <andrew@opentechlabs.co.uk>

---
+++ tools/passcrypt.c
@@ -0,0 +1,47 @@
+/*
+ * passcrypt.c
+ *
+ * Copyright (C) 2011		OpenTech Labs
+ * 				Andrew Clayton <andrew@opentechlabs.co.uk>
+ * Released under the GNU General Public License (GPL) version 3.
+ * See COPYING
+ */
+
+#define _XOPEN_SOURCE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <glib.h>
+
+int main(int argc, char **argv)
+{
+	static const char salt_chars[64] =
+"./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char salt[21];
+	int i;
+
+	if (argc < 4) {
+		printf("Usage: passcrypt -l <256/512> <password>\n");
+		exit(EXIT_FAILURE);
+	}
+
+	if (strcmp(argv[2], "256") == 0) {
+		strcpy(salt, "$5$");
+	} else if (strcmp(argv[2], "512") == 0) {
+		strcpy(salt, "$6$");
+	} else {
+		printf("Unknown SHA type.\n");
+		exit(EXIT_FAILURE);
+	}
+
+	for (i = 3; i < 19; i++)
+		salt[i] = salt_chars[g_random_int_range(0, 64)];
+
+	strcat(salt, "$");
+	printf("%s\n", crypt(argv[3], salt));
+
+	exit(EXIT_SUCCESS);
+}
