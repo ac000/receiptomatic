ac000/receiptomatic

commit 030b50c2d3cbd50023c8a4a11142b80d73b81692

utils: Convert get_vars to strtok_r
Signed-off-by: Andrew Clayton <andrew@opentechlabs.co.uk>

www/utils.c
@@ -214,36 +214,37 @@ void free_avars(GList *avars)
  */
 GHashTable *get_vars(char *query)
 {
-	int i;
-	int j = 0;
-	int str_len;
-	char buf[255];
-	char key[255];
-	char *val;
+	char *string;
+	char *token;
+	char *subtoken;
+	char *saveptr1 = NULL;
+	char *saveptr2 = NULL;
 	GHashTable *query_values;
 
 	query_values = g_hash_table_new_full(g_str_hash, g_str_equal,
 							g_free, g_free);
 
-	memset(buf, 0, 255);
-	str_len = strlen(query);
-	for (i = 0; i <= str_len; i++) {
-		if (query[i] == '=') {
-			strcpy(key, buf);
-			memset(buf, 0, 255);
-			j = 0;
-		} else if (query[i] == '&' || query[i] == '\0') {
-			val = url_decode(buf);
-			d_fprintf(debug_log, "Adding key: %s with value: %s "
-						"to hash table\n", key, val);
-			g_hash_table_replace(query_values, g_strdup(key),
-								g_strdup(val));
-			memset(buf, 0, 255);
-			free(val);
-			j = 0;
-		} else {
-			buf[j++] = query[i];
-		}
+	string = strdupa(query);
+	for (;;) {
+		char *key;
+		char *value;
+
+		token = strtok_r(string, "&", &saveptr1);
+		if (token == NULL)
+			break;
+
+		subtoken = strtok_r(token, "=", &saveptr2);
+		key = subtoken;
+		token = NULL;
+		subtoken = strtok_r(token, "=", &saveptr2);
+		value = url_decode(subtoken);
+
+		d_fprintf(debug_log, "Adding key: %s with value: %s to hash "
+							"table\n", key, value);
+		g_hash_table_replace(query_values, g_strdup(key),
+							g_strdup(value));
+
+		string = NULL;
 	}
 
 	return query_values;

commit 49e6509af79a3c58e32b8548f6a00406c9523617

utils: Fix get_vars
Commit 030b50c2 broke the POSTing of variables without any values, we
would segfault on the value subtoken being NULL. Fix this by making it
behave the way it did before the conversion to strtok_r.

If a variable has no value with it, store it as an empty string. How
this didn't get noticed at the time is a mystery.

Signed-off-by: Andrew Clayton <andrew@opentechlabs.co.uk>

www/utils.c
@@ -237,7 +237,10 @@ GHashTable *get_vars(char *query)
 		key = subtoken;
 		token = NULL;
 		subtoken = strtok_r(token, "=", &saveptr2);
-		value = url_decode(subtoken);
+		if (subtoken == NULL)
+			*value = '\0';
+		else
+			value = url_decode(subtoken);
 
 		d_fprintf(debug_log, "Adding key: %s with value: %s to hash "
 							"table\n", key, value);