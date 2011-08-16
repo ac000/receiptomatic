d21b0a1d6a2c63b6d62f0b64055f84d5ecf93e3e
Andrew Clayton <andrew@opentechlabs.co.uk>
2011-08-16 09:58:09 +0100

    Convert /approve_receipts/ to send_template()
    Signed-off-by: Andrew Clayton <andrew@opentechlabs.co.uk>

--- www/url_handlers.c
+++ www/url_handlers.c
@@ -1690,10 +1690,7 @@ static void approve_receipts(struct session *current_session, GHashTable *qvars)
 	ml = TMPL_add_loop(ml, "table", loop);
 
 out:
-	printf("Content-Type: text/html\r\n\r\n");
-	TMPL_write("templates/approve_receipts.tmpl", NULL, NULL, ml, stdout,
-								error_log);
-	fflush(error_log);
+	send_template("templates/approve_receipts.tmpl", ml);
 	TMPL_free_varlist(ml);
 	mysql_free_result(res);
 	mysql_close(conn);
