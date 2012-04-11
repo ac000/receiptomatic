00be69ba8cbe305e2003a0e6783e08af52ed20f2
Andrew Clayton <andrew@opentechlabs.co.uk>
2012-04-11 15:01:31 +0100

    url_handlers: Make header file #defines consistent
    s/_URLHANDLERS_H_/_URL_HANDLERS_H_/g
    
    Signed-off-by: Andrew Clayton <andrew@opentechlabs.co.uk>

--- www/url_handlers.h
+++ www/url_handlers.h
@@ -7,11 +7,11 @@
  * See COPYING
  */
 
-#ifndef _URLHANDLERS_H_
-#define _URLHANDLERS_H_
+#ifndef _URL_HANDLERS_H_
+#define _URL_HANDLERS_H_
 
 #define MAX_RECEIPT_AGE (60 * 60 * 24 * 180)      /* 180 days */
 
 void handle_request(void);
 
-#endif /* _URLHANDLERS_H_ */
+#endif /* _URL_HANDLERS_H_ */
