6d743e64282fa3ff00fb6c8dedba7ea834187060
Andrew Clayton <andrew@opentechlabs.co.uk>
2012-04-20 15:21:20 +0100

    url_handlers: Convert prefs_edit_user() to IS_APPROVER()
    Signed-off-by: Andrew Clayton <andrew@opentechlabs.co.uk>

--- www/url_handlers.c
+++ www/url_handlers.c
@@ -1320,7 +1320,7 @@ static void prefs_edit_user(void)
 
 	if (user_session.capabilities & ADMIN)
 		vl = TMPL_add_var(vl, "admin", "yes", (char *)NULL);
-	if (user_session.capabilities & APPROVER)
+	if (IS_APPROVER())
 		vl = TMPL_add_var(vl, "approver", "yes", (char *)NULL);
 
 	vl = TMPL_add_var(vl, "user_hdr", user_session.user_hdr, (char *)NULL);
