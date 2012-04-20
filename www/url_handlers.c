15c0acb35e7f0a27c35dcd5bf2175d24fd36398f
Andrew Clayton <andrew@opentechlabs.co.uk>
2012-04-20 15:17:54 +0100

    url_handlers: Convert prefs() to IS_APPROVER()
    Signed-off-by: Andrew Clayton <andrew@opentechlabs.co.uk>

--- www/url_handlers.c
+++ www/url_handlers.c
@@ -1064,7 +1064,7 @@ static void prefs(void)
 
 	if (user_session.capabilities & ADMIN)
 		vl = TMPL_add_var(vl, "admin", "yes", (char *)NULL);
-	if (user_session.capabilities & APPROVER)
+	if (IS_APPROVER())
 		vl = TMPL_add_var(vl, "approver", "yes", (char *)NULL);
 
 	vl = TMPL_add_var(vl, "user_hdr", user_session.user_hdr, (char *)NULL);
