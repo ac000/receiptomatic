11d238572ddc464527ed1ba0e76e5818ef3ce776
Andrew Clayton <andrew@opentechlabs.co.uk>
2012-04-20 15:31:50 +0100

    url_handlers: Convert reviewed_receipts() to IS_APPROVER()
    Signed-off-by: Andrew Clayton <andrew@opentechlabs.co.uk>

--- www/url_handlers.c
+++ www/url_handlers.c
@@ -1845,14 +1845,14 @@ static void reviewed_receipts(void)
 	TMPL_loop *loop = NULL;
 	TMPL_fmtlist *fmtlist;
 
-	if (!(user_session.capabilities & APPROVER))
+	if (!IS_APPROVER())
 		return;
 
 	if (qvars)
 		get_page_pagination(get_var(qvars, "page_no"), GRID_SIZE,
 							&page, &from);
 
-	if (user_session.capabilities & APPROVER)
+	if (IS_APPROVER())
 		ml = TMPL_add_var(ml, "approver", "yes", (char *)NULL);
 	if (user_session.capabilities & ADMIN)
 		ml = TMPL_add_var(ml, "admin", "yes", (char *)NULL);
