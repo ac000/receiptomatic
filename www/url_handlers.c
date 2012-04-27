d6b71291f7076347e7752744a179e525914da388
Andrew Clayton <andrew@opentechlabs.co.uk>
2012-04-27 10:16:25 +0100

    url_handlers: Switch forgotten_password() to KEY_EXP
    Signed-off-by: Andrew Clayton <andrew@opentechlabs.co.uk>

--- www/url_handlers.c
+++ www/url_handlers.c
@@ -998,7 +998,7 @@ static void forgotten_password(void)
 	tm = time(NULL);
 	snprintf(sql, SQL_MAX, "INSERT INTO activations VALUES ('%s', '%s', "
 							"%ld)", email_addr,
-							key, tm + 86400);
+							key, tm + KEY_EXP);
 	d_fprintf(sql_log, "%s\n", sql);
 	mysql_real_query(conn, sql, strlen(sql));
 
