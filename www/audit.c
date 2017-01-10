ac000/receiptomatic

commit 45f7d638d7abeb55c69a4c861ded4304d8b74d59

www: Fix potentially slow logins
When a user logs in we do a reverse DNS lookup of the users IP address
to get the hostname. We store both the IP address and hostname (if any)
of the requesting client in the utmp table. This is used in the 'Last
login' line we display. Also having both the IP address _and_ hostname
provides a better audit trail.

Normally this should only take a fraction of a second. However there are
cases where we get a 2(SERVFAIL) after a timeout. If there was just no
PTR record then we'd get a 3(NXDOMAIN) pretty quickly.

Seeing as there is no need to delay the login just to get the hostname,
we can do the IP->hostname lookup in a thread which will do the lookup
and update the utmp table accordingly based on the clients session id.

We move the hostname lookup out of log_login() and into a thread routine
log_utmp_host() which is created from log_login. This allows the login
to complete right away and the utmp table is updated asynchronously.

Signed-off-by: Andrew Clayton <andrew@digital-domain.net>

www/audit.c
@@ -20,6 +20,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <pthread.h>
 
 #include <ctemplate.h>
 
@@ -138,6 +139,45 @@ out3:
 	return login_ok;
 }
 
+struct utmp_info {
+	char ip[INET6_ADDRSTRLEN];	/* IP address of client */
+	unsigned long long sid;		/* Session ID of client */
+};
+
+/*
+ * Thread to lookup the hostname of the client IP address and update
+ * the umtp table at login.
+ *
+ * This is done in a separate thread as it can sometimes take a number
+ * of seconds to complete and there's no need to hold up the login for it.
+ */
+static void *log_utmp_host(void *arg)
+{
+	char *hostname;
+	char host[NI_MAXHOST] = "\0";
+	struct addrinfo hints;
+	struct addrinfo *res;
+	struct utmp_info *ui = (struct utmp_info *)arg;
+	MYSQL *db;
+
+	memset(&hints, 0, sizeof(struct addrinfo));
+	hints.ai_family = AF_UNSPEC;
+	getaddrinfo(ui->ip, NULL, &hints, &res);
+	getnameinfo(res->ai_addr, res->ai_addrlen, host, NI_MAXHOST, NULL, 0,
+			0);
+
+	db = db_conn();
+	hostname = make_mysql_safe_string(host);
+	sql_query("UPDATE utmp SET hostname = '%s' WHERE sid = %llu",
+			hostname, ui->sid);
+	mysql_close(db);
+	free(hostname);
+	free(ui);
+	freeaddrinfo(res);
+
+	return NULL;
+}
+
 /*
  * Add a login entry to the utmp table.
  *
@@ -147,32 +187,24 @@ out3:
 unsigned long long log_login(void)
 {
 	char *username;
-	char *hostname;
 	char *ip_addr;
-	char host[NI_MAXHOST] = "\0";
-	struct addrinfo hints;
-	struct addrinfo *ares;
 	struct timespec login_at;
+	struct utmp_info *ui;
 	unsigned long long sid;
 	unsigned int uid;
 	MYSQL_RES *res;
 	MYSQL_ROW row;
+	pthread_t tid;
+	pthread_attr_t attr;
 
 	clock_gettime(CLOCK_REALTIME, &login_at);
 
-	memset(&hints, 0, sizeof(struct addrinfo));
-	hints.ai_family = AF_UNSPEC;
-	getaddrinfo(env_vars.remote_addr, NULL, &hints, &ares);
-	getnameinfo(ares->ai_addr, ares->ai_addrlen, host, NI_MAXHOST, NULL, 0,
-			0);
-
 	username = make_mysql_safe_string(get_var(qvars, "username"));
-	hostname = make_mysql_safe_string(host);
 	ip_addr = make_mysql_safe_string(env_vars.remote_addr);
 	res = sql_query("SELECT uid FROM passwd WHERE username = '%s'",
 			username);
 	row = mysql_fetch_row(res);
-	uid = atoi(row[0]);
+	uid = strtoul(row[0], NULL, 10);
 	mysql_free_result(res);
 
 	/* We need to be sure a new sid isn't inserted here */
@@ -184,17 +216,26 @@ unsigned long long log_login(void)
 
 	/* Divide tv_nsec by 1000 to get a rough microseconds value */
 	sql_query("INSERT INTO utmp VALUES (%ld.%06ld, %u, '%s', '%s', %d, "
-			"'%s', %llu)",
+			"'', %llu)",
 			login_at.tv_sec, login_at.tv_nsec / NS_USEC,
-			uid, username, ip_addr, env_vars.remote_port,
-			hostname, sid);
+			uid, username, ip_addr, env_vars.remote_port, sid);
 	sql_query("UNLOCK TABLES");
 
 	mysql_free_result(res);
 	free(username);
-	free(hostname);
 	free(ip_addr);
-	freeaddrinfo(ares);
+
+	/*
+	 * ui is free'd in the log_utmp_host thread as it will need to
+	 * exist beyond the life of this function.
+	 */
+	ui = malloc(sizeof(struct utmp_info));
+	snprintf(ui->ip, sizeof(ui->ip), "%s", env_vars.remote_addr);
+	ui->sid = sid;
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+	pthread_create(&tid, &attr, log_utmp_host, (void *)ui);
+	pthread_attr_destroy(&attr);
 
 	return sid;
 }

www/Makefile
@@ -1,7 +1,7 @@
 CC=gcc
 CFLAGS=-Wall -std=c99 -O2 -g -Wp,-D_FORTIFY_SOURCE=2 -D_FILE_OFFSET_BITS=64 -D_HAVE_FCGX -fexceptions -fstack-protector --param=ssp-buffer-size=4 -fPIE
 LDFLAGS=-Wl,-z,now -pie
-LIBS=-L../../libctemplate `pkg-config --libs glib-2.0` `pkg-config --libs gmime-2.6` `mysql_config --libs` -lm -lrt -lcrypt -lfcgi -lmhash -ltokyocabinet -lmagic -lctemplate-fcgx
+LIBS=-L../../libctemplate `pkg-config --libs glib-2.0` `pkg-config --libs gmime-2.6` `mysql_config --libs` -lm -lrt -lcrypt -lfcgi -lmhash -ltokyocabinet -lmagic -lctemplate-fcgx -pthread
 INCS=-I../../libctemplate `pkg-config --cflags glib-2.0` `pkg-config --cflags gmime-2.6` `mysql_config --cflags`
 
 objects = receiptomatic-www.o get_config.o url_handlers.o url_helpers.o data_extraction.o utils.o db.o audit.o

commit 4404fc530ae5a3036808dd7db1165968c9be9880

www: audit: Fix a segfault when doing name lookups
Commit 45f7d63 ("www: Fix potentially slow logins") introduced a fix for
doing name lookups on the clients IP address at login time where this
could sometimes take a few seconds delaying login. That fix moved the
name lookup into its own thread.

However that introduced a bug. The thread was messing with the global
MYSQL *conn handle, I was seeing this problem manifest itself by a MySQL
query returning no results when it clearly should have, e.g login and
immediately logout and in again we would sometimes get a segfault where
the "SELECT uid, name, capabilities FROM passwd WHERE username =
'EMAIL' sql query from create_session() (after we have successfully
logged in) would return 0 results as can be seen from this backtrace
snippet

  #0  0x00007f7bc59d1a51 in mysql_num_fields (res=res@entry=0x0)
      at /usr/src/debug/mariadb-5.5.52/sql-common/client.c:4363
  #1  0x00007f7bc6c50d8f in get_dbrow (res=res@entry=0x0) at utils.c:554
  #2  0x00007f7bc6c52fa2 in create_session (sid=20) at audit.c:468
  #3  0x00007f7bc6c47f0d in login () at url_handlers.c:61
  #4  0x00007f7bc6c44123 in uri_map (uri_handler=uri_handler@entry=0x7f7bc6c47e50 <login>, uri=0x7f7bc6c54a83 "/login/") at url_handlers.c:2266
  #5  0x00007f7bc6c4c552 in handle_request (uri_handler=0x7f7bc6c47e50 <login>, uri=0x7f7bc6c54a83 "/login/") at url_handlers.c:2367
  #6  0x00007f7bc6c4c552 in handle_request () at url_handlers.c:2306
  #7  0x00007f7bc6c4512c in create_server () at receiptomatic-www.c:426
  #8  0x00007f7bc6c4512c in create_server (nr=<optimized out>)
      at receiptomatic-www.c:447
  #9  0x00007f7bc6c444b0 in main (argc=<optimized out>, argv=<optimized out>)
      at receiptomatic-www.c:530

res=res@entry=0x0 is the result of above sql query.

To fix this, we need to use a db connection that is private to the
log_utmp_host() thread. To do this we create a new db_conn_local()

www/audit.c
@@ -314,9 +314,9 @@ static void *log_utmp_host(void *arg)
 	getnameinfo(res->ai_addr, res->ai_addrlen, host, NI_MAXHOST, NULL, 0,
 			0);
 
-	db = db_conn();
-	hostname = make_mysql_safe_string(host);
-	sql_query("UPDATE utmp SET hostname = '%s' WHERE sid = %llu",
+	db = db_conn_local();
+	hostname = make_mysql_safe_stringl(db, host);
+	sql_queryl(db, "UPDATE utmp SET hostname = '%s' WHERE sid = %llu",
 			hostname, ui->sid);
 	mysql_close(db);
 	free(hostname);

www/common.h
@@ -141,11 +141,28 @@
  * Given a string it will return a string, that must be free'd, that is safe
  * to pass to mysql.
  */
+static inline char *__make_mysql_safe_string(MYSQL *dbconn, const char *string)
+{
+	char *safe = malloc(strlen(string)*2 + 1);
+
+	mysql_real_escape_string(dbconn, safe, string, strlen(string));
+	return safe;
+}
+
+/*
+ * Warppaer around __make_mysql_safe_string() using the global db connection.
+ */
 static inline char *make_mysql_safe_string(const char *string)
 {
-	char *safe = malloc(strlen(string) * 2 + 1);
-	mysql_real_escape_string(conn, safe, string, strlen(string));
-	return safe;
+	return __make_mysql_safe_string(conn, string);
+}
+
+/*
+ * Warppaer around __make_mysql_safe_string() using the local db connection.
+ */
+static inline char *make_mysql_safe_stringl(MYSQL *dbconn, const char *string)
+{
+	return __make_mysql_safe_string(dbconn, string);
 }
 
 /*

www/db.c
@@ -25,6 +25,8 @@
 /* Global MySQL connection handle */
 MYSQL *conn;
 
+enum { DB_CONN_GLOBAL, DB_CONN_LOCAL };
+
 char *db_host = "localhost";
 char *db_socket_name = NULL;
 unsigned int db_port_num = 3306;
@@ -33,8 +35,9 @@ unsigned int db_flags = 0;
 /*
  * Opens up a MySQL connection and returns the connection handle.
  */
-MYSQL *db_conn(void)
+static MYSQL *__db_conn(int db_conn_type)
 {
+	MYSQL *dbc;
 	MYSQL *ret;
 
 	if (MULTI_TENANT) {
@@ -46,22 +49,42 @@ MYSQL *db_conn(void)
 		free(db_name);
 		db_name = strdup(db);
 	}
-	conn = mysql_init(NULL);
-	ret = mysql_real_connect(conn, DB_HOST, DB_USER, DB_PASS, DB_NAME,
+
+	dbc = mysql_init(NULL);
+	ret = mysql_real_connect(dbc, DB_HOST, DB_USER, DB_PASS, DB_NAME,
 			DB_PORT_NUM, DB_SOCKET_NAME, DB_FLAGS);
 
 	if (!ret) {
 		d_fprintf(error_log, "Failed to connect to database. Error: "
-				"%s\n", mysql_error(conn));
-		switch (mysql_errno(conn)) {
+				"%s\n", mysql_error(dbc));
+		switch (mysql_errno(dbc)) {
 		case ER_BAD_DB_ERROR:	/* unknown database */
 			send_template("templates/invalid.tmpl", NULL, NULL);
 			break;
 		}
-		conn = NULL;
+		dbc = NULL;
 	}
 
-	return conn;
+	if (db_conn_type == DB_CONN_GLOBAL)
+		conn = dbc;
+
+	return dbc;
+}
+
+/*
+ * Wrapper around __db_conn() to open a new global db connection.
+ */
+MYSQL *db_conn(void)
+{
+	return __db_conn(DB_CONN_GLOBAL);
+}
+
+/*
+ * Wrapper around __db_conn() to open a new local db connection.
+ */
+MYSQL *db_conn_local(void)
+{
+	return __db_conn(DB_CONN_LOCAL);
 }
 
 /*
@@ -75,11 +98,12 @@ MYSQL *db_conn(void)
  * This function will either return a result set or NULL. Note that some
  * queries don't return result sets by design.
  */
-MYSQL_RES *__sql_query(const char *func, const char *fmt, ...)
+MYSQL_RES *__sql_query(MYSQL *dbconn, const char *func, const char *fmt, ...)
 {
 	va_list args;
 	char sql[SQL_MAX];
 	int len;
+	MYSQL *dbc = conn;
 
 	va_start(args, fmt);
 	len = vsnprintf(sql, sizeof(sql), fmt, args);
@@ -98,6 +122,9 @@ MYSQL_RES *__sql_query(const char *func, const char *fmt, ...)
 		fflush(sql_log);
 	}
 
-	mysql_real_query(conn, sql, len);
-	return mysql_store_result(conn);
+	if (dbconn)
+		dbc = dbconn;
+
+	mysql_real_query(dbc, sql, len);
+	return mysql_store_result(dbc);
 }

www/db.h
@@ -4,6 +4,8 @@
  * Copyright (C) 2011-2013	OpenTech Labs
  *				Andrew Clayton <andrew@digital-domain.net>
  *
+ * 		 2016		Andrew Clayton <andrew@digital-domain.net>
+ *
  * Released under the GNU Affero General Public License version 3.
  * See COPYING
  */
@@ -28,11 +30,18 @@
  * along with the function name of the caller for the sql log.
  */
 #define sql_query(fmt, ...) \
-	__sql_query((const char *)__func__, fmt, ##__VA_ARGS__)
+	__sql_query(NULL, (const char *)__func__, fmt, ##__VA_ARGS__)
+
+/*
+ * Same as the above, but takes a MYSQL * for the connection to operate on.
+ */
+#define sql_queryl(dbconn, fmt, ...) \
+	__sql_query(dbconn, (const char *)__func__, fmt, ##__VA_ARGS__)
 
 extern MYSQL *conn;
 
 MYSQL *db_conn(void);
-MYSQL_RES *__sql_query(const char *func, const char *fmt, ...);
+MYSQL *db_conn_local(void);
+MYSQL_RES *__sql_query(MYSQL *dbconn, const char *func, const char *fmt, ...);
 
 #endif /* _DB_H_ */